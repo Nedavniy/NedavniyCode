fe2da2809aaad0e8d4dd1ed16e2c47b76c1739bb
Nedavniy <nedavniidanylo@gmail.com>
2025-09-09 19:33:39 +0200

    chore: update components and styles

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,7 +2,7 @@
         {
             id: 0,
             title: 'NedavniyCode',
-            images: '',
+            image: 'https://github.com/Nedavniy/NedavniyCode/blob/main/src/assets/images/NedavniyCode.png?raw=true',
             description: 'Personal portfolio of Danil Nedavniy, showcasing web development projects, skills in React, HTML, CSS, and JavaScript, and experience building modern, responsive interfaces.',
             stack: ['React', 'HTML', 'CSS', 'JS'],
             github: 'https://github.com/Nedavniy/NedavniyCode'
@@ -10,7 +10,7 @@
         {
             id: 1,
             title: 'ToDo App',
-            images: '',
+            image: '',
             description: 'Simple ToDo App built with vanilla JavaScript, HTML, and CSS',
             stack: ['HTML', 'CSS', 'JS'],
             github: 'https://github.com/Nedavniy/ToDo-App'
@@ -18,7 +18,7 @@
         {
             id: 2,
             title: 'Counter-App',
-            images: '',
+            image: '',
             description: 'Simple Counter App built with HTML, CSS, and JavaScript. Interactive UI with increment, decrement, and reset functionality.',
             stack: ['HTML', 'CSS', 'JS'],
             github: 'https://github.com/Nedavniy/Counter-App',
