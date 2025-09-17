b7667a7c9019ad3277ff5fd7cb15f6e5b69d35b2
Nedavniy <nedavniidanylo@gmail.com>
2025-09-17 17:43:19 +0200

    feat: improve Contact component layout and input styles

---
+++ src/components/Contact.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+  return (
+    <section className='contact__section container'>
+      <div className='contact__heading--div'>
+        <h2 className='heading__h2'>
+          Contact <span className='modify__blue'>me</span>
+        </h2>
+      </div>
+
+      <div className='contact'>
+        <form className="contact-form" action="#" method="POST">
+
+          <div className="contact-row">
+            <div className="contact-field">
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+
+            <div className="contact-field">
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="message">Your Message</label>
+            <textarea 
+              placeholder='Message' 
+              id="message" 
+              name="message" 
+              required
+            ></textarea>
+          </div>
+
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Hero from '../components/Hero.jsx'
 import About from '../components/About.jsx'
 import Portfolio from '../components/Portfolio.jsx'
+import Contact from '../components/Contact.jsx'
 function Home() {
 
     return (
@@ -12,6 +13,7 @@ function Home() {
                 <Hero />
                 <About />
                 <Portfolio />
+                <Contact />
             </main>
         </>
     )
